dec9c518d392e2fc92ffe98365ed66286920435b
Wemerson <nerdhits001@gmail.com>
2024-05-25 11:15:28 -0300

    starting structure of the challenge

---
+++ src/main/java/com/example/picpay_backend_challenge/PicpayBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.picpay_backend_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayBackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/picpay_backend_challenge/PicpayBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picpay_backend_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
