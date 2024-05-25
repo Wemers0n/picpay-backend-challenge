64f939513693408e84fd92168554a21aef52ba96
Wemerson <nerdhits001@gmail.com>
2024-05-25 11:49:44 -0300

    feat: create user entity

---
+++ src/main/java/com/example/picpay_backend_challenge/domain/user/User.java
@@ -0,0 +1,31 @@
+package com.example.picpay_backend_challenge.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
---
+++ src/main/java/com/example/picpay_backend_challenge/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package com.example.picpay_backend_challenge.domain.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
