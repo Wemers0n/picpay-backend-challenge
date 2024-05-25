50555fe32b39ad2fdbee8cc3d60634b4cbcb7fd9
Wemerson <nerdhits001@gmail.com>
2024-05-25 11:50:22 -0300

    feat: create transaction entity

---
+++ src/main/java/com/example/picpay_backend_challenge/domain/transaction/Transaction.java
@@ -0,0 +1,32 @@
+package com.example.picpay_backend_challenge.domain.transaction;
+
+import com.example.picpay_backend_challenge.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+}
